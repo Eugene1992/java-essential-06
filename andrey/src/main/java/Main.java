2092c2e349d4730f641ef21df41cba30667e31a9
a-telegin <telegin.andrii@gmail.com>
2016-08-16 11:03:24 +0300

    Another test commit with loop print

--- andrey/src/main/java/Main.java
+++ andrey/src/main/java/Main.java
@@ -1,11 +1,12 @@
-/**
- * Created by evgeniy on 16/08/16.
- */
+
 public class Main {
     public static void main(String[] args){
         System.out.println("Test");
         System.out.println("Just another test String");
         System.out.println("String");
+        for (int i=0; i<3 ; i++){
+            System.out.println(i);
+        }
     }
 
 }
