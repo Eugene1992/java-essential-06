7f23ed272f3f390136b43bffea8777a2edc989a8
a-telegin <telegin.andrii@gmail.com>
2016-08-16 11:00:56 +0300

    Another test commit

--- andrey/src/main/java/Main.java
+++ andrey/src/main/java/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args){
         System.out.println("Test");
         System.out.println("Just another test String");
+        System.out.println("String");
     }
 
 }
