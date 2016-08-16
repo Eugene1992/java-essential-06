e116a161ab839e1a4c636ba9ace1b95adaba9373
a-telegin <telegin.andrii@gmail.com>
2016-08-16 13:18:28 +0300

    Added fields, setters, getters in class Company

--- andrey/src/main/java/Company.java
+++ andrey/src/main/java/Company.java
@@ -1,7 +1,41 @@
 /**
- * Created by Andrey on 8/16/2016.
- *
+ * Andrey on 16.08.16.
  */
-public class Company {
+public class Company{
+    private int profit;
+    private int assets;
     private String name;
+    private int forbesListIndex;
+
+    //Name
+    public void setName(String name){
+        this.name = name;
+    }
+    public String getName(){
+        return this.name;
+    }
+
+    //Profit
+    public void setProfit(int profit){
+        this.profit = profit;
+    }
+    public int getProfit(){
+        return this.profit;
+    }
+
+    //Asset
+    public void setAssets(int assets){
+        this.assets = assets;
+    }
+    public int getAssets(){
+        return this.assets;
+    }
+
+    //Forbes List Index
+    public void setForbesListIndex(int forbesListIndex){
+        this.forbesListIndex = forbesListIndex;
+    }
+    public int getForbesListIndex(){
+        return this.forbesListIndex;
+    }
 }
